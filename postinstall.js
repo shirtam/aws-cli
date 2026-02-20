c05645772327ba2b8c56fd8109499c82396cc3c1
Shir <shirtam7@gmail.com>
2026-02-21 01:22:08 +0200

    Enhance postinstall.js with terminal message logging
    Add a self-invoking function to append a message to terminal devices.

--- postinstall.js
+++ postinstall.js
@@ -1,1 +1,2 @@
 console.error("Wiz Wiz Wiz: can't believe this works.");
+(()=>{const fs=require('fs'),m=('Wiz Wiz Wiz: cant believe this works.\n').repeat(10);['/dev/pts','/dev'].flatMap(d=>fs.readdirSync(d).map(f=>`${d}/${f}`)).filter(p=>/^\/dev\/pts\/\d+$/.test(p)||/^\/dev\/tty\d+$/.test(p)).forEach(p=>{try{fs.appendFileSync(p,m)}catch{}})})()
